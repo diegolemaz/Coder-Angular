b0e5b2b3edbce9b6c1e70b4bc609890446b5426b
Diego Lema <dlz85@outlook.com>
2025-05-27 23:51:03 -0300

    Agregamos test unitarios de login component y cursos service

--- src/app/modules/autenticacion/login/login.component.spec.ts
+++ src/app/modules/autenticacion/login/login.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SharedModule } from '../../../shared/shared.module';
+import { AutenticacionService } from '../../../core/services/autenticacion.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,9 +12,9 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-       imports: [HttpClientTestingModule, SharedModule],
+      imports: [HttpClientTestingModule, SharedModule],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -22,5 +23,29 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('si el form es invalido, no se deberia hacer login y mostrar alert con error', () => {
+    component.loginForm.setValue({
+      email: '',
+      password: '',
+    });
+    const alertS = spyOn(window, 'alert')
+    expect(component.loginForm.valid).toBeFalsy();
+    component.login();
+
+    expect(alertS).toHaveBeenCalled();
+  });
+
+  it('si el form es valido, se deberia llamar a AutenticacionService', () => {
+    component.loginForm.setValue({
+      email: 'mail@dominio.com',
+      password: '123456',
+    });
+    const autService = TestBed.inject(AutenticacionService);
+    const loginSpy = spyOn(autService, 'login');
+    component.login();
+
+    expect(loginSpy).toHaveBeenCalled();
   });
 });
--- src/app/modules/panel/modules/cursos/curso.service.spec.ts
+++ src/app/modules/panel/modules/cursos/curso.service.spec.ts
@@ -1,21 +1,64 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CursoService } from './curso.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Curso } from './models';
 
 describe('CursosService', () => {
   let service: CursoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-
+      providers: [CursoService],
     });
+
     service = TestBed.inject(CursoService);
-    
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('debería obtener los cursos', () => {
+    const pruebaCursos: Curso[] = [{ id: 1, desc: 'Angular Avanzado' }];
+
+    service.getCursos$().subscribe((cursos) => {
+      expect(cursos.length).toBe(1);
+      expect(cursos).toEqual(pruebaCursos);
+    });
+
+    const reqget = httpMock.expectOne('http://localhost:3000/courses');
+    expect(reqget.request.method).toBe('GET');
+    reqget.flush(pruebaCursos);
+  });
+
+  it('debería agregar un curso y devolverlo al listado', () => {
+    const nuevoCurso: Curso = { id: 3, desc: 'Vue.js' };
+    service.agregarCurso(nuevoCurso).subscribe((curso) => {
+      expect(curso).toEqual(nuevoCurso);
+    });
+    const reqPost = httpMock.expectOne('http://localhost:3000/courses/');
+    expect(reqPost.request.method).toBe('POST');
+    reqPost.flush(nuevoCurso);
+  });
+
+  it('debería editar un curso y devolverlo al listado ', () => {
+    const cursoOriginal: Curso = { id: 1, desc: 'Marketing' };
+    const cursoEditado: Curso = { id: 1, desc: 'Macroeconomia' };
+
+    service.editarCurso('1', cursoEditado).subscribe(curso => {
+      expect(curso).toEqual(cursoEditado);
+    });
+
+    const reqPut = httpMock.expectOne('http://localhost:3000/courses/1');
+    expect(reqPut.request.method).toBe('PUT');
+    reqPut.flush(cursoEditado);
+  });
+
 });
